a20a8915cf5759bab61c94d6dc88649ef1eda912
Johannes Harmse <johannesharmse91@gmail.com>
2018-04-14 15:50:30 -0700

    Add test cases

--- tests/testthat/test_auth_NLU.R
+++ tests/testthat/test_auth_NLU.R
@@ -13,6 +13,30 @@ credentials <- readRDS("credentials.rds")
 username <- credentials$username
 password <- credentials$password
 
-test_that("Check if authorization works.", {
-  expect_equal(auth_NLU(username = username, password = password), "Valid credentials provided.")
+test_that("Check error handling for missing input", {
+
+  expect_error(with_config({auth_NLU(username = NULL, password = password)}, override = TRUE),
+               "Please specify a valid username and password combination as string arguments.")
+})
+
+test_that("Check error handling for non-character input", {
+  expect_error(auth_NLU(username = username, password = as.factor(password)),
+               "Please specify a valid username and password combination as string arguments.")
+})
+
+# can't clear cache
+
+# test_that("Check error handling for if valid credential authorization works.", {
+#   cache <- ".httr-oauth"
+#   if (file.exists(cache)) file.remove(cache)
+#   # revoke_all()
+#   set_config(authenticate(username, "XXXXX"), override = TRUE)
+#   with_config(authenticate(username, "XXXXX"), expect_equal(auth_NLU(username = username, password = "XXXXX"),
+#                "Invalid credentials provided."), override = TRUE)
+#   reset_config()
+# })
+
+test_that("Check if valid credential authorization works.", {
+  expect_equal(auth_NLU(username = username, password = password),
+               "Valid credentials provided.")
 })
