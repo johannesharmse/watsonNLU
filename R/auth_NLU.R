9982bdd62ac1a731dfef5890259de91c3731d0eb
Johannes Harmse <johannesharmse91@gmail.com>
2018-04-13 18:10:20 -0700

    Clean code

--- R/auth_NLU.R
+++ R/auth_NLU.R
@@ -28,15 +28,7 @@ auth_NLU <- function(username = NULL, password=NULL){
     stop("Please specify a valid username and password combination as string arguments.")
   }
     # base login url
-    # url_NLU="https://gateway.watsonplatform.net/natural-language-understanding/api"
-    # or try this:
     url_NLU = "https://gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze"
-    # url_NLU="https://gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze?version=2017-02-27"
-
-
-    # authenticate(username, password)
-
-    # instead of doing full GET, would it make sense to do a post?
 
     response <- GET(url=paste0(url_NLU),
          authenticate(username, password),
@@ -52,23 +44,5 @@ auth_NLU <- function(username = NULL, password=NULL){
     }else{
       stop(content(response))
     }
-
-    # return(status_code(test))
-
-    # print(response)
-
-    # GET -- opens channel with credentials
-    # response <- GET(url=url_NLU,
-    #   authenticate(username,password),
-    #   add_headers("Content-Type"="application/json")
-    # )
-
-    # check for successful response
-  #   if (status_code(response) != 200){
-  #     message(response)
-  #     stop("Please make sure your username and password combination is correct
-  #        and that you have a valid internet connection or check the response log above.")
-  # }
-  # # }
 }
 
