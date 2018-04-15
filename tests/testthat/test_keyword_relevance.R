953fa3d345fd38980feb255b56909f1e8feb636d
Johannes Harmse <johannesharmse91@gmail.com>
2018-04-15 10:25:10 -0700

    Add keyword relevance test cases

--- tests/testthat/test_keyword_relevance.R
+++ tests/testthat/test_keyword_relevance.R
@@ -12,6 +12,12 @@ credentials <- readRDS("credentials.rds")
 username <- credentials$username
 password <- credentials$password
 
+text_input <- readLines(con = "test.txt")
+url_input <- "http://www.nytimes.com/guides/well/how-to-be-happy"
+
+response_success <- keyword_relevance(input = text_input, input_type='text', limit = 3)
+response_success_url <- keyword_relevance(input = url_input, input_type='url', limit = 5)
+
 # authenticate user
 auth_NLU(username = username, password = password)
 
@@ -27,19 +33,33 @@ test_that("Check error handling for non-character input", {
                "Please specify input text or URL as a character string")
 })
 
-# can't clear cache
+test_that("Check error handling for unaccepted input types", {
+  expect_error(keyword_relevance(input = text_input, input_type='html', limit = 1),
+               "Input type should be either 'url' or 'text'.")
+})
 
-# test_that("Check error handling for if valid credential authorization works.", {
-#   cache <- ".httr-oauth"
-#   if (file.exists(cache)) file.remove(cache)
-#   # revoke_all()
-#   set_config(authenticate(username, "XXXXX"), override = TRUE)
-#   with_config(authenticate(username, "XXXXX"), expect_equal(auth_NLU(username = username, password = "XXXXX"),
-#                "Invalid credentials provided."), override = TRUE)
-#   reset_config()
-# })
+test_that("Check error handling for evidently short input.", {
+  expect_error(keyword_relevance(input = "abc", input_type='text', limit = 1),
+               "Please make sure you have a valid internet connection and provided a valid input. Check the response log above for further details.")
+})
 
-test_that("Check if valid credential authorization works.", {
-  expect_equal(auth_NLU(username = username, password = password),
-               "Valid credentials provided.")
+test_that("Check that number of results is controlled by limit argument.", {
+  expect_equal(nrow(response_success),
+               3)
+})
+
+test_that("Check correct number of columns.", {
+  expect_equal(ncol(response_success),
+               2)
+})
+
+test_that("Check that all relevance scores are between 0 and 1.", {
+  expect_equal(nrow(response_success[response_success$relevance >= 0 &
+                                  response_success$relevance <= 1, ]),
+               nrow(response_success))
+})
+
+test_that("Check for url input successful response", {
+  expect_equal(nrow(response_success_url),
+               5)
 })
